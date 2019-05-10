19bfd0ebe0f59684aece8fc703003abb4b0496f3
kalimkhan7 <kalimullah.khan@raweng.com>
2019-05-10 10:09:54 +0530

    first commit

---
+++ task4.js
@@ -0,0 +1,663 @@
+
+
+    var JSONdata = [
+        {
+        "id": "38172",
+        "employee_name": "test23",
+        "employee_salary": "20",
+        "employee_age": "10",
+        "profile_image": ""
+        },
+        {
+        "id": "38173",
+        "employee_name": "bhat54434",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38174",
+        "employee_name": "ss1234",
+        "employee_salary": "333",
+        "employee_age": "33",
+        "profile_image": ""
+        },
+        {
+        "id": "38185",
+        "employee_name": "test123",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38188",
+        "employee_name": "testresult",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38193",
+        "employee_name": "ssss",
+        "employee_salary": "2123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38196",
+        "employee_name": "testhansraajwww",
+        "employee_salary": "0",
+        "employee_age": "0",
+        "profile_image": ""
+        },
+        {
+        "id": "38200",
+        "employee_name": "test12345",
+        "employee_salary": "123",
+        "employee_age": "24",
+        "profile_image": ""
+        },
+        {
+        "id": "38201",
+        "employee_name": "",
+        "employee_salary": "0",
+        "employee_age": "0",
+        "profile_image": ""
+        },
+        {
+        "id": "38202",
+        "employee_name": "btf1244g",
+        "employee_salary": "11223",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38203",
+        "employee_name": "user2",
+        "employee_salary": "254",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38204",
+        "employee_name": "user3",
+        "employee_salary": "254",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38205",
+        "employee_name": "bhat5444",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38206",
+        "employee_name": "Amlslyeggst",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38207",
+        "employee_name": "user4",
+        "employee_salary": "254",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38208",
+        "employee_name": "tJJKKJJest",
+        "employee_salary": "1212321323",
+        "employee_age": "45",
+        "profile_image": ""
+        },
+        {
+        "id": "38209",
+        "employee_name": "testvishal",
+        "employee_salary": "9634",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38210",
+        "employee_name": "AAAAAAA",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38211",
+        "employee_name": "Ram11",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38212",
+        "employee_name": "bhat54",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38214",
+        "employee_name": "Ram20",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38215",
+        "employee_name": "bhat44",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38216",
+        "employee_name": "test4",
+        "employee_salary": "1234",
+        "employee_age": "123",
+        "profile_image": ""
+        },
+        {
+        "id": "38218",
+        "employee_name": "test5",
+        "employee_salary": "1234",
+        "employee_age": "123",
+        "profile_image": ""
+        },
+        {
+        "id": "38219",
+        "employee_name": "BBBBB",
+        "employee_salary": "1230",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38172",
+        "employee_name": "test23",
+        "employee_salary": "20",
+        "employee_age": "10",
+        "profile_image": ""
+        },
+        {
+        "id": "38173",
+        "employee_name": "bhat54434",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38174",
+        "employee_name": "ss1234",
+        "employee_salary": "333",
+        "employee_age": "33",
+        "profile_image": ""
+        },
+        {
+        "id": "38185",
+        "employee_name": "test123",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38188",
+        "employee_name": "testresult",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38193",
+        "employee_name": "ssss",
+        "employee_salary": "2123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38196",
+        "employee_name": "testhansraajwww",
+        "employee_salary": "0",
+        "employee_age": "0",
+        "profile_image": ""
+        },
+        {
+        "id": "38200",
+        "employee_name": "test12345",
+        "employee_salary": "123",
+        "employee_age": "24",
+        "profile_image": ""
+        },
+        {
+        "id": "38201",
+        "employee_name": "",
+        "employee_salary": "0",
+        "employee_age": "0",
+        "profile_image": ""
+        },
+        {
+        "id": "38202",
+        "employee_name": "btf1244g",
+        "employee_salary": "11223",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38203",
+        "employee_name": "user2",
+        "employee_salary": "254",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38204",
+        "employee_name": "user3",
+        "employee_salary": "254",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38205",
+        "employee_name": "bhat5444",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38206",
+        "employee_name": "Amlslyeggst",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38207",
+        "employee_name": "user4",
+        "employee_salary": "254",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38208",
+        "employee_name": "tJJKKJJest",
+        "employee_salary": "1212321323",
+        "employee_age": "45",
+        "profile_image": ""
+        },
+        {
+        "id": "38209",
+        "employee_name": "testvishal",
+        "employee_salary": "9634",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38210",
+        "employee_name": "AAAAAAA",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38211",
+        "employee_name": "Ram11",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38212",
+        "employee_name": "bhat54",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38214",
+        "employee_name": "Ram20",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38215",
+        "employee_name": "bhat44",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38216",
+        "employee_name": "test4",
+        "employee_salary": "1234",
+        "employee_age": "123",
+        "profile_image": ""
+        },
+        {
+        "id": "38218",
+        "employee_name": "test5",
+        "employee_salary": "1234",
+        "employee_age": "123",
+        "profile_image": ""
+        },
+        {
+        "id": "38219",
+        "employee_name": "BBBBB",
+        "employee_salary": "1230",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38172",
+        "employee_name": "test23",
+        "employee_salary": "20",
+        "employee_age": "10",
+        "profile_image": ""
+        },
+        {
+        "id": "38173",
+        "employee_name": "bhat54434",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38174",
+        "employee_name": "ss1234",
+        "employee_salary": "333",
+        "employee_age": "33",
+        "profile_image": ""
+        },
+        {
+        "id": "38185",
+        "employee_name": "test123",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38188",
+        "employee_name": "testresult",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38193",
+        "employee_name": "ssss",
+        "employee_salary": "2123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38196",
+        "employee_name": "testhansraajwww",
+        "employee_salary": "0",
+        "employee_age": "0",
+        "profile_image": ""
+        },
+        {
+        "id": "38200",
+        "employee_name": "test12345",
+        "employee_salary": "123",
+        "employee_age": "24",
+        "profile_image": ""
+        },
+        {
+        "id": "38201",
+        "employee_name": "",
+        "employee_salary": "0",
+        "employee_age": "0",
+        "profile_image": ""
+        },
+        {
+        "id": "38202",
+        "employee_name": "btf1244g",
+        "employee_salary": "11223",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38203",
+        "employee_name": "user2",
+        "employee_salary": "254",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38204",
+        "employee_name": "user3",
+        "employee_salary": "254",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38205",
+        "employee_name": "bhat5444",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38206",
+        "employee_name": "Amlslyeggst",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38207",
+        "employee_name": "user4",
+        "employee_salary": "254",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38208",
+        "employee_name": "tJJKKJJest",
+        "employee_salary": "1212321323",
+        "employee_age": "45",
+        "profile_image": ""
+        },
+        {
+        "id": "38209",
+        "employee_name": "testvishal",
+        "employee_salary": "9634",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38210",
+        "employee_name": "AAAAAAA",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38211",
+        "employee_name": "Ram11",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38212",
+        "employee_name": "bhat54",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38214",
+        "employee_name": "Ram20",
+        "employee_salary": "123",
+        "employee_age": "23",
+        "profile_image": ""
+        },
+        {
+        "id": "38215",
+        "employee_name": "bhat44",
+        "employee_salary": "123",
+        "employee_age": "25",
+        "profile_image": ""
+        },
+        {
+        "id": "38216",
+        "employee_name": "test4",
+        "employee_salary": "1234",
+        "employee_age": "123",
+        "profile_image": ""
+        },
+        {
+        "id": "38218",
+        "employee_name": "test5",
+        "employee_salary": "1234",
+        "employee_age": "123",
+        "profile_image": ""
+        },
+        {
+        "id": "38219",
+        "employee_name": "BBBBB",
+        "employee_salary": "1230",
+        "employee_age": "23",
+        "profile_image": ""
+        }
+    ]
+
+var data =  generateId(JSONdata);
+function generateId(arr){
+    index = 1;
+    var data = [];
+    for(let object of arr){
+        object.id = index;
+        
+        data.push(object)
+        index++;
+    }
+    return data;
+}
+
+window.onload = function() {
+    createHeader(tableHeader);
+    changePage(1);
+};
+
+var tableHeader = Object.keys(data[0]);
+var table = document.querySelector("table");
+var tbody = table.createTBody();
+
+function createHeader(arr){
+    var thead = table.createTHead();
+    var row = thead.insertRow();
+
+    for (let element of tableHeader) {
+        let th = document.createElement("th");
+        let text = element.charAt(0).toUpperCase() + element.replace(/_/g, " ").slice(1);
+        th.innerText = text;
+        row.appendChild(th);
+    }
+}
+
+var currentPage = 1;
+var recordsPerPage  = 10;
+
+function previousPage(){
+    if(currentPage > 1){
+        currentPage--;
+        changePage(currentPage);
+    }
+}
+
+function nextPage(){
+   
+    if(currentPage < numberOfPages()){
+        currentPage++;
+        changePage(currentPage);
+        
+    }
+}
+
+var isSearched = false;
+
+function changePage(page){
+    var page_span = document.getElementById("page");
+    page_span.innerHTML = page + "/" + numberOfPages();
+
+    if(isSearched){
+        createTable(searchResult,page);
+        toggle(searchResult,page);
+    } else {
+        createTable(data,page);
+        toggle(data,page);
+    }  
+}
+
+
+function toggle(arr,page){
+    var btnNext = document.getElementById("btn_next");
+    var btnPrevious = document.getElementById("btn_prev");
+
+    if(arr.length <= recordsPerPage ) {
+        btnNext.style.visibility = "hidden";
+        btnPrevious.style.visibility = "hidden";
+    } else if (page == 1){
+        btnNext.style.visibility = "visible";
+        btnPrevious.style.visibility = "hidden";
+    } else if (page == numberOfPages()) {
+        btnNext.style.visibility = "hidden";
+        btnPrevious.style.visibility = "visible";
+    } else {
+        btnNext.style.visibility = "visible";
+        btnPrevious.style.visibility = "visible";
+    }
+}
+var searchResult = [];
+
+function search(){
+    searchResult = [];
+    var input = document.getElementById("input");
+    var filter = input.value.toUpperCase();
+    var page_span = document.getElementById("page");
+   
+    for(var i = 0; i < data.length; i++){
+                
+        if(data[i].employee_name.toUpperCase().indexOf(filter) != -1) searchResult.push(data[i]);
+        if(searchResult){
+            isSearched = true;
+            page_span.innerHTML = currentPage + "/" + numberOfPages();
+            currentPage = 1;
+            createTable(searchResult,currentPage);
+            toggle(searchResult,currentPage);
+        }
+    }
+}
+
+
+
+function numberOfPages()
+{
+    if(isSearched){
+        return Math.ceil(searchResult.length / recordsPerPage);
+    } else {
+        return Math.ceil(data.length / recordsPerPage);
+    }
+    
+}
+
+function createTable(arr, page){
+    tbody.innerHTML = "";      
+    for (let i=(page-1)*recordsPerPage; i < page*recordsPerPage;i++) {
+        if(arr[i]) {
+            var row = tbody.insertRow();
+            for (var j = 0; j < tableHeader.length; j++) {
+                var td = row.insertCell();
+                td.innerHTML = arr[i][tableHeader[j]];     
+            }
+        }   
+    }
+}
+
